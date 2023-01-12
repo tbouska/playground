174f9336725544bafefc8ff10f727b539ede9599
Tomáš Bouška <tomas@buvis.net>
2023-01-12 21:19:00 +0100

    Add polls list

--- svelte/ninjapolls/src/App.svelte
+++ svelte/ninjapolls/src/App.svelte
@@ -2,11 +2,45 @@
   import Header from './lib/Header.svelte'
   import Footer from './lib/Footer.svelte'
   import Tabs from './lib/Tabs.svelte'
+  import CreatePollForm from './lib/CreatePollForm.svelte'
+  import PollList from './lib/PollList.svelte'
 
   const items = ["Current polls", "Create new poll"]
   let activeItem = "Current polls";
+  let polls = [
+    {
+      id: 1,
+      question: "Python or JavaScript?",
+      answerA: "Python",
+      answerB: "JavaScript",
+      votesA: 15,
+      votesB: 9
+    }
+  ];
+
   const switchTab = (e) => {
     activeItem = e.detail;
+  }
+
+  const handleAddNewPoll = (e) => {
+    const newPoll = e.detail;
+    polls = [newPoll, ...polls];
+    console.log(JSON.stringify(polls));
+    activeItem = "Current polls";
+  }
+
+  const handleVote = (e) => {
+    const {answer, id} = e.detail;
+    let copiedPolls = [...polls];
+    let upvotedPoll = copiedPolls.find((item) => item.id ===id)
+    if (answer === "a") {
+      upvotedPoll.votesA++;
+    }
+    if (answer === "b") {
+      upvotedPoll.votesB++;
+    }
+    polls = copiedPolls;
+
   }
 </script>
 
@@ -14,9 +48,9 @@
 <main>
   <Tabs {items} {activeItem} on:switchTab={switchTab} />
   {#if activeItem === "Current polls"}
-    <p>List of polls</p>
+    <PollList {polls} on:vote={handleVote} />
   {:else if activeItem === "Create new poll"}
-    <p>Form to create a new poll</p>
+    <CreatePollForm on:addNewPoll={handleAddNewPoll} />
   {/if}
 </main>
   <Footer />
---
+++ svelte/ninjapolls/src/lib/CreatePollForm.svelte
@@ -0,0 +1,113 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  import Button from "./Button.svelte"
+
+  let fields = { question: "", answerA: "", answerB: "" };
+  let errors = { question: "", answerA: "", answerB: "" };
+  let valid = false;
+  let submitAttempt = false;
+
+  const dispatch = createEventDispatcher();
+
+  $: {
+    fields = fields;
+    if (submitAttempt) {
+      validateForm();
+    }
+  }
+
+  const validateForm = () => {
+    valid = true;
+
+    if (fields.question.trim().length < 5) {
+      valid = false;
+      errors.question = "Question must be at least 5 characters long";
+    } else {
+      errors.question = "";
+    }
+
+    if (fields.answerA.trim().length < 1) {
+      valid = false;
+      errors.answerA = "Answer A cannot be empty";
+    } else {
+      errors.answerA = "";
+    }
+
+    if (fields.answerB.trim().length < 1) {
+      valid = false;
+      errors.answerB = "Answer B cannot be empty";
+    } else {
+      errors.answerB = "";
+    }
+  }
+
+  const submitHandler = () => {
+    submitAttempt = true;
+    validateForm();
+
+    if (valid) {
+      let newPoll = {id: Math.random()*101|0, ...fields, votesA: 0, votesB: 0}
+      dispatch('addNewPoll', newPoll);
+    }
+  }
+</script>
+
+<form on:submit|preventDefault={submitHandler}>
+  <div class="form-field">
+    <label for="question">Poll Question:</label>
+    <input type="text" id="question" bind:value={fields.question}>
+    <div class="error">{errors.question}</div>
+  </div>
+  <div class="form-field">
+    <label for="answer-a">Answer A:</label>
+    <input type="text" id="answer-a" bind:value={fields.answerA}>
+    <div class="error">{errors.answerA}</div>
+  </div>
+  <div class="form-field">
+    <label for="answer-b">Answer B:</label>
+    <input type="text" id="answer-b" bind:value={fields.answerB}>
+    <div class="error">{errors.answerB}</div>
+  </div>
+  <div class="form-footer">
+    <Button flat type="secondary">Add poll</Button>
+  </div>
+</form>
+
+<style>
+  form {
+    width: 400px;
+    margin: 0 auto;
+  }
+
+  .form-field {
+    margin: 18px auto;
+    text-align: center;
+  }
+
+  input {
+    width: 100%;
+    border-radius: 6px;
+    border: 2px solid #555;
+    padding: 0.3rem;
+    font-size: 0.9rem;
+  }
+
+  label {
+    color: #555;
+    margin: 10px auto;
+    display: block;
+    text-align: left;
+  }
+
+  .error {
+    margin-top: 0.6rem;
+    font-weight: bold;
+    font-size: 0.8rem;;
+    color: #d91b42;
+  }
+
+  .form-footer {
+    text-align: center;
+    margin-top: 2.5rem;
+  }
+</style>
