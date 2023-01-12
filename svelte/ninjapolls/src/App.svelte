4361168c529f9a3fff6f72f4d40b1459ec4d41ab
Tomáš Bouška <tomas@buvis.net>
2023-01-12 18:34:44 +0100

    Add tabs

--- svelte/ninjapolls/src/App.svelte
+++ svelte/ninjapolls/src/App.svelte
@@ -1,9 +1,29 @@
 <script>
+  import Header from './lib/Header.svelte'
+  import Footer from './lib/Footer.svelte'
+  import Tabs from './lib/Tabs.svelte'
+
+  const items = ["Current polls", "Create new poll"]
+  let activeItem = "Current polls";
+  const switchTab = (e) => {
+    activeItem = e.detail;
+  }
 </script>
 
+<Header />
 <main>
-  <h1>Hello, Ninjas!</h1>
+  <Tabs {items} {activeItem} on:switchTab={switchTab} />
+  {#if activeItem === "Current polls"}
+    <p>List of polls</p>
+  {:else if activeItem === "Create new poll"}
+    <p>Form to create a new poll</p>
+  {/if}
 </main>
+  <Footer />
 
 <style>
+  main {
+    max-width: 960px;
+    margin: 40px auto;
+  }
 </style>
---
+++ svelte/ninjapolls/src/lib/Footer.svelte
@@ -0,0 +1,19 @@
+<footer>
+  <div class="copyright">Copyright 2023 Tomáš Bouška</div>
+</footer>
+
+<style>
+  footer {
+    text-align: center;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    border-top: 1px solid #ddd;
+  }
+  .copyright {
+    color: #aaa;
+    font-size: 14px;
+    display: inline-block;
+    padding: 20px;
+  }
+</style>
