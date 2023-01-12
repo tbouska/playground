e59c64b2fc8b92b248b5a356a539dc933107845e
Tomáš Bouška <tomas@buvis.net>
2023-01-12 12:06:39 +0100

    Add Svelte Tutorial for Beginners

---
+++ svelte/svelte-tutorial-for-beginners/src/App.svelte
@@ -0,0 +1,42 @@
+<script>
+  import Modal from './Modal.svelte';
+
+  let showModal = false;
+  let people = [
+    { name: "yoshi", beltColor: "black", age: 25, id: 1 },
+    { name: "mario", beltColor: "orange", age: 45, id: 2 },
+    { name: "luigi", beltColor: "brown", age: 35, id: 3 },
+  ];
+
+  const toggleModal = () => showModal = !showModal;
+
+  const handleClick = (id) => {
+    people = people.filter((person) => person.id != id);
+  }
+</script>
+
+
+<Modal {showModal} on:click={toggleModal}>
+  <h3>Add a new person</h3>
+  <form>
+    <input type="text" placeholder="name">
+    <input type="text" placeholder="belt color">
+    <button>Add Person</button>
+  </form>
+</Modal>
+<main>
+  <button on:click={toggleModal}>Open Modal</button>
+  {#each people as person (person.id)}
+    <div>
+      <h4>{person.name}</h4>
+      {#if person.beltColor === "black"}
+        <p><strong>MASTER NINJA</strong></p>
+      {/if}
+      <p>{person.age} years old, {person.beltColor} belt.</p>
+      <button on:click={() => handleClick(person.id)}>delete</button>
+    </div>
+  {/each}
+</main>
+
+<style>
+</style>
---
+++ svelte/svelte-tutorial-for-beginners/src/Modal.svelte
@@ -0,0 +1,26 @@
+<script>
+  export let showModal = false;
+</script>
+
+{#if showModal}
+  <div class="backdrop" on:click|self>
+    <div class="modal">
+      <slot />
+    </div>
+  </div>
+{/if}
+
+<style>
+  .backdrop {
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    background: rgba(0,0,0,0.8);
+  }
+  .modal {
+    width: 50rem;
+    margin: 100px auto;
+    background: white;
+    border-radius:  15px;
+  }
+</style>
