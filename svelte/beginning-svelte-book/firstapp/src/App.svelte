05bd0cfa7c1540b0f4e7daea71e6c2beb694eabe
Tomáš Bouška <tomas@buvis.net>
2023-01-16 19:29:03 +0100

    Implement JumboTron

--- svelte/beginning-svelte-book/firstapp/src/App.svelte
+++ svelte/beginning-svelte-book/firstapp/src/App.svelte
@@ -1,7 +1,12 @@
 <script>
-  import Products from './lib/Products.svelte'
+  import Products from "./lib/Products.svelte";
+  import JumboTron from "./lib/JumboTron.svelte";
 </script>
 
 <main>
-  <Products />
+  <!--<Products />-->
+  <JumboTron>
+    This is a long sentence, and I want to insert content into the
+    jumbotron component from the outside.
+  </JumboTron>
 </main>
