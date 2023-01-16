1ae108d2654beaca2f0648de4677077c26e07531
Tomáš Bouška <tomas@buvis.net>
2023-01-16 19:17:31 +0100

    Conditional rendering

--- svelte/beginning-svelte-book/firstapp/src/App.svelte
+++ svelte/beginning-svelte-book/firstapp/src/App.svelte
@@ -1,12 +1,7 @@
 <script>
   import Products from './lib/Products.svelte'
-  import Rating from './lib/Rating.svelte'
-  import { Button } from 'sveltestrap'
 </script>
 
 <main>
   <Products />
-  <Button color="danger">Click Me</Button>
-  <Rating rating="1" />
-  <Rating rating="3" />
 </main>
