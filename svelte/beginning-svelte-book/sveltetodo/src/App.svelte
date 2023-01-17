17a31a01675aa5b0a668f636ef83d5856a4de24a
Tomáš Bouška <tomas@buvis.net>
2023-01-17 21:05:38 +0100

    Add svelte todo

---
+++ svelte/beginning-svelte-book/sveltetodo/src/App.svelte
@@ -0,0 +1,7 @@
+<script>
+  import ToDoList from "./lib/ToDoList.svelte";
+</script>
+
+<main>
+  <ToDoList />
+</main>
---
+++ svelte/beginning-svelte-book/sveltetodo/src/lib/ToDoList.svelte
@@ -0,0 +1,35 @@
+<script>
+  import { TodoStore } from "../stores.js";
+  import { Table, Button } from "sveltestrap";
+
+  const handleDelete = (todoId) => {
+    TodoStore.update(currentTodos => {
+      return currentTodos.filter(todo => todo.id != todoId)
+    });
+  };
+</script>
+
+<Table>
+  <thead>
+    <tr>
+      <th>To Do</th>
+      <th>Date Created</th>
+      <th>Complete</th>
+      <th>Edit</th>
+      <th>Delete</th>
+    </tr>
+  </thead>
+  <tbody>
+  {#each $TodoStore as todo (todo.id)}
+    <tr>
+      <td>{todo.text}</td>
+      <td>{todo.date}</td>
+      <td>Mark Complete</td>
+      <td>Edit</td>
+      <td>
+        <Button on:click={() => handleDelete(todo.id)} color="danger">Delete</Button>
+      </td>
+    </tr>
+  {/each}
+  </tbody>
+</Table>
