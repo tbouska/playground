7b4526c395cac82f39baffc841cc72c822c870b9
Tomáš Bouška <tomas@buvis.net>
2023-01-18 20:47:42 +0100

    Add reactivity

--- svelte/beginning-svelte-book/sveltetodo/src/App.svelte
+++ svelte/beginning-svelte-book/sveltetodo/src/App.svelte
@@ -2,9 +2,14 @@
   import ToDoList from "./lib/ToDoList.svelte";
   import ToDoForm from "./lib/ToDoForm.svelte";
   import ToDoEditForm from "./lib/ToDoEditForm.svelte";
+  import { TodoStore } from "./stores.js";
 
   let editMode = false;
   let editTodo = null;
+
+  $: count = $TodoStore.length;
+  $: countPending = $TodoStore.filter(todo => todo.complete === false).length;
+  $: countCompleted = $TodoStore.filter(todo => todo.complete === true).length;
 
   const handleEditTodo = (event) => {
     editMode = true;
@@ -22,6 +27,9 @@
     <ToDoForm />
   </div>
   {/if}
+  <br />
+  <h4>Total Todos: {count} | Pending: {countPending} | Completed: {countCompleted}</h4>
+  <br />
   <ToDoList on:edit-todo={handleEditTodo} />
 </main>
 
--- svelte/beginning-svelte-book/sveltetodo/src/lib/ToDoList.svelte
+++ svelte/beginning-svelte-book/sveltetodo/src/lib/ToDoList.svelte
@@ -3,6 +3,7 @@
   import { Table, Button } from "sveltestrap";
   import { createEventDispatcher, onMount } from "svelte";
   import axios from "axios";
+  import { scale } from "svelte/transition";
 
   const endpoint = "http://localhost:4000/todos/"
 
@@ -19,6 +20,24 @@
   });
 
   const dispatch = createEventDispatcher();
+
+  const handleToggleComplete = async (todo) => {
+    todo.complete = !todo.complete;
+    if(todo.complete === true){
+      todo.dateCompleted = new Date().toDateString();
+    } else {
+      todo.dateCompleted = false;
+    }
+
+    await axios.patch(endpoint + todo.id,{complete: todo.complete, dateCompleted: todo.dateCompleted});
+
+    TodoStore.update(currentTodos => {
+      let updatedTodos = structuredClone(currentTodos);  // to avoid text reset after store update, when editTodo.text is emptied
+      let updatedTodo = updatedTodos.find((item) => item.id === todo.id);
+      updatedTodo = todo;
+      return updatedTodos;
+    });
+  };
 
   const handleDelete = async (todoId) => {
     await axios.delete(endpoint + todoId);
@@ -45,17 +64,52 @@
   </thead>
   <tbody>
   {#each $TodoStore as todo (todo.id)}
-    <tr>
-      <td>{todo.text}</td>
-      <td>{todo.date}</td>
-      <td>Mark Complete</td>
-      <td>
-        <Button on:click={() => handleEdit(todo)}>Edit</Button>
-      </td>
-      <td>
-        <Button on:click={() => handleDelete(todo.id)} color="danger">Delete</Button>
-      </td>
-    </tr>
+    {#if !todo.complete}
+      <tr out:scale>
+        <td>{todo.text}</td>
+        <td>{todo.date}</td>
+        <td>
+          <Button on:click={() => handleToggleComplete(todo)} color="success">Mark Complete</Button>
+        </td>
+        <td>
+          <Button on:click={() => handleEdit(todo)}>Edit</Button>
+        </td>
+        <td>
+          <Button on:click={() => handleDelete(todo.id)} color="danger">Delete</Button>
+        </td>
+      </tr>
+    {/if}
   {/each}
   </tbody>
 </Table>
+
+{#if $TodoStore.filter(todo => todo.complete === true).length > 0}
+<br />
+<h2>Completed tasks</h2>
+<Table>
+  <thead>
+    <tr>
+      <th>To Do</th>
+      <th>Date Completed</th>
+      <th>Complete</th>
+      <th>Delete</th>
+    </tr>
+  </thead>
+  <tbody>
+  {#each $TodoStore as todo (todo.id)}
+    {#if todo.complete}
+      <tr out:scale>
+        <td>{todo.text}</td>
+        <td>{todo.dateCompleted}</td>
+        <td>
+          <Button on:click={() => handleToggleComplete(todo)} color="success">Mark Incomplete</Button>
+        </td>
+        <td>
+          <Button on:click={() => handleDelete(todo.id)} color="danger">Delete</Button>
+        </td>
+      </tr>
+    {/if}
+  {/each}
+  </tbody>
+</Table>
+{/if}
